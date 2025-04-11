f6bfc14571f99c8f66b8804952642152602a063d
Flavia Kosti <flaviakosti@gmail.com>
2025-04-10 22:46:30 -0400

    Mini-Widget-Dashboard: Calculator Widget

--- app.js
+++ app.js
@@ -171,3 +171,29 @@ function updateAnalogClock() {
 
 setInterval(updateAnalogClock, 1000);
 updateAnalogClock(); // initial call
+
+
+let display = document.getElementById("calc-display");
+let buttons = document.querySelectorAll(".calculator .btn");
+
+buttons.forEach(button => {
+	button.addEventListener("click", () => {
+		const value = button.dataset.value;
+
+		if (value) {
+			display.value += value;
+		}
+
+		if (button.id === "clear") {
+			display.value = "";
+		}
+
+		if (button.id === "equals") {
+			try {
+				display.value = eval(display.value);
+			} catch {
+				display.value = "Error";
+			}
+		}
+	});
+});
