6f37f11d373a0f0c17fd42c62c19f95bb404c12e
Flavia Kosti <flaviakosti@gmail.com>
2025-04-11 23:36:19 -0400

    Mini-Widget-Dashboard: Style P1

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const todoList = document.getElementById('todo-list');
 fetch('https://jsonplaceholder.typicode.com/todos')
 	.then(response => response.json())
 	.then(data => {
-		todoList.innerHTML = data.slice(0, 5).map(todo => `<li>${todo.title}</li>`).join('');
+		todoList.innerHTML = data.sort(() => Math.random() - 0.5).slice(0, 5).map(todo => `<li class="flex-row"><span>${todo.completed ? '\u{1F7E9}' : '\u2B1C'}</span> <span>${todo.title}</span></li>`).join('');
 	})
 	.catch(error => {
 		todoList.innerHTML = "<li>Failed to load todo list.</li>";
