e6609d556c7c6d1044d670ee18f16df9a2a6b022
Flavia Kosti <flaviakosti@gmail.com>
2025-04-18 23:03:59 -0400

    Mini-Widget-Dashboard: Code Clean Up P6

--- app.js
+++ app.js
@@ -5,32 +5,13 @@ import { getToDoItems } from './scripts/todo-widget.js';
 import { bmiCalculator } from './scripts/bmi-calculator-widget.js';
 import { getRandomPetImage } from './scripts/pet-image-widget.js';
 import { getRandomJoke } from './scripts/joke-widget.js';
+import { getNasaImage } from './scripts/nasa-widget.js';
 
 
 
 
 
 
-
-// NASA Image of the Day
-const nasaImage = document.getElementById('nasa-image');
-const nasaTitle = document.getElementById('nasa-title');
-const nasaDescription = document.getElementById('nasa-description');
-
-function getNasaImage() {
-	const handleResponse = (data) => {
-		nasaImage.src = data && data.media_type === "image" ? data.url : '/assets/images/earth.webp';
-		nasaTitle.textContent = data.title || 'Failed to load';
-		nasaDescription.textContent = data.explanation;
-	}
-
-	fetch(`https://api.nasa.gov/planetary/apod?api_key=${import.meta.env.VITE_NASA_API}`)
-		.then(res => res.ok ? res.json() : null)
-		.then(data => handleResponse(data))
-		.catch(() => {
-			nasaTitle.textContent = "Failed to load NASA image.";
-		});
-}
 
 // Random Fact - Useless Facts API
 const factButton = document.getElementById('fact-button');
---
+++ scripts/nasa-widget.js
@@ -0,0 +1,19 @@
+// NASA Image of the Day
+
+export function getNasaImage() {
+	const nasaImage = document.getElementById('nasa-image');
+	const nasaTitle = document.getElementById('nasa-title');
+	const nasaDescription = document.getElementById('nasa-description');
+	const handleResponse = (data) => {
+		nasaImage.src = data && data.media_type === "image" ? data.url : '/assets/images/earth.webp';
+		nasaTitle.textContent = data.title || 'Failed to load';
+		nasaDescription.textContent = data.explanation;
+	}
+
+	fetch(`https://api.nasa.gov/planetary/apod?api_key=${import.meta.env.VITE_NASA_API}`)
+		.then(res => res.ok ? res.json() : null)
+		.then(data => handleResponse(data))
+		.catch(() => {
+			nasaTitle.textContent = "Failed to load NASA image.";
+		});
+}
