77218be3b544f99b1c5dba1175eeae0a9c6518ed
Flavia Kosti <flaviakosti@gmail.com>
2025-04-20 18:38:05 -0400

    Mini-Widget-Dashboard: Art of the Day

--- scripts/art-of-day.js
+++ scripts/art-of-day.js
@@ -2,7 +2,6 @@ export async function loadArtOfTheDay() {
 	const img = document.getElementById('art-image');
 	const title = document.getElementById('art-title');
 	const artist = document.getElementById('art-artist');
-	const date = document.getElementById('art-date');
 
 	try {
 		const response = await fetch('https://api.artic.edu/api/v1/artworks?page=1&limit=100');
@@ -14,9 +13,8 @@ export async function loadArtOfTheDay() {
 		const imageUrl = `https://www.artic.edu/iiif/2/${imageId}/full/843,/0/default.jpg`;
 
 		img.src = imageUrl;
-		title.textContent = art.title || 'Untitled';
+		title.textContent = `"${art.title}"` || 'Untitled';
 		artist.textContent = art.artist_title || 'Unknown Artist';
-		date.textContent = art.date_display || '';
 
 	} catch (error) {
 		console.error('Error loading Art of the Day:', error);
