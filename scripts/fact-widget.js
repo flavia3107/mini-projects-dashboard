23fb7e9655963c8ebf027854dbedb3292bfb6562
Flavia Kosti <flaviakosti@gmail.com>
2025-04-28 19:52:41 -0400

    Mini-Widget-Dashboard: Code Clean Up P3

--- scripts/fact-widget.js
+++ scripts/fact-widget.js
@@ -7,15 +7,13 @@ export function getRandomFact() {
 	randomFactCallback();
 }
 
-function randomFactCallback() {
+async function randomFactCallback() {
 	const factText = document.getElementById('fact-text');
 	const handleResponse = (data) => {
-		const quote = data.text || FUN_fACTS[Math.floor(Math.random() * FUN_fACTS.length)];
+		const quote = data?.text || FUN_fACTS[Math.floor(Math.random() * FUN_fACTS.length)];
 		factText.textContent = `${quote.split(" - ")[0]}`;
 	}
-
-	fetch('https://uselessfacts.jsph.pl/api/v2/facts/random?language=en')
-		.then(res => res.json())
-		.then(data => handleResponse(data))
-		.catch(() => handleResponse());
+	const response = await fetch('https://uselessfacts.jsph.pl/api/v2/facts/random?language=en');
+	const data = response.ok ? await response.json() : null;;
+	handleResponse(data);
 }
